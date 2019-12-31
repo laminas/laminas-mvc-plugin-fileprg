fdfd5e3f5fbf922fa34fdf9cf8b73f4cdb1b3012
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:33:43 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/SampleController.php
+++ test/TestAsset/SampleController.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
- * @link      http://github.com/zendframework/zend-mvc-plugin-fileprg for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-mvc-plugin-fileprg for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc-plugin-fileprg/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc-plugin-fileprg/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Mvc\Plugin\FilePrg\TestAsset;
+namespace LaminasTest\Mvc\Plugin\FilePrg\TestAsset;
 
-use Zend\Mvc\Controller\AbstractActionController;
+use Laminas\Mvc\Controller\AbstractActionController;
 
 class SampleController extends AbstractActionController
 {
